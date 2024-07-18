88216e44970ad427e3bb0b69621985a1c9434ff7
Your Name <manzi5000@hotmail.co.uk>
2024-07-18 17:08:12 +0100

    error handling for delete

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
+
 const users = require("./users.json");
 
+users.forEach((user) => {
+  user.id = Math.round(Math.random() * 10000);
+});
+
 app.get("/", (request, response) => {
-  const _users = [...users];
-  _users.forEach((user) => {
-    user.id = Math.round(Math.random() * 100);
-  });
   response.send(users);
 });
 
+//====================================
+//delete one
 app.delete("/:id", (request, response) => {
   console.log("new delete request", request.params.id);
   const { id } = request.params;
@@ -18,10 +21,24 @@ app.delete("/:id", (request, response) => {
     return user.id === Number(id);
   });
 
+  if (id < 0 || Number.isNaN(id)) {
+    //this fails for NaN?!
+    response.status(400).send("Incorrect ID");
+    return;
+  }
+
+  if (id === NaN) {
+    //also fails
+    response.status(400).send("ID doesn't exist");
+  }
+
   users.splice(indexOf, 1);
 
   response.send(users);
 });
+
+//====================================
+//Write One
 
 //====================================
 
